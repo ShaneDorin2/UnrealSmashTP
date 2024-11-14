ae358b7c5136b5bfbb2f34f4c25262314b3edf43
ShaneDorin <shane.dorin@edu.devinci.fr>
2024-11-14 09:53:34 +0100

    Input Mapping WIP

---
+++ SmashUE_ClearedVersion-master/Source/SmashUE/Public/Characters/SmashCharacterSettings.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "SmashCharacterSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SMASHUE_API USmashCharacterSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+};

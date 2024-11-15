7ca7c79bdea67cf01f20243a4af22d780459d7de
ShaneDorin <shane.dorin@edu.devinci.fr>
2024-11-15 12:06:10 +0100

    Jump/Fall states wip

---
+++ SmashUE_ClearedVersion-master/Source/SmashUE/Public/Characters/States/SmashCharacterStateJump.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/SmashCharacterState.h"
+#include "SmashCharacterStateJump.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class SMASHUE_API USmashCharacterStateJump : public USmashCharacterState
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	USmashCharacterStateJump();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};

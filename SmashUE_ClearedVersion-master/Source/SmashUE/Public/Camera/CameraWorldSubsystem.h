a346207c315dabf1ee7ee67fb0905c24d8d79f35
ShaneDorin <shane.dorin@edu.devinci.fr>
2024-11-21 11:20:34 +0100

    CameraWIP

---
+++ SmashUE_ClearedVersion-master/Source/SmashUE/Public/Camera/CameraWorldSubsystem.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "CameraWorldSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SMASHUE_API UCameraWorldSubsystem : public UTickableWorldSubsystem
+{
+	GENERATED_BODY()
+};

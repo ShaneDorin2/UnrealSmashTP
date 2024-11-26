d712a22cd6608ff879818de4b6cb38b7b0ec2760
ShaneDorin <shane.dorin@edu.devinci.fr>
2024-11-26 12:10:38 +0100

    Local multiplayer wip

--- SmashUE_ClearedVersion-master/Source/LocalMultiplayer/Private/LocalMultiplayerSubsystem.cpp
+++ SmashUE_ClearedVersion-master/Source/LocalMultiplayer/Private/LocalMultiplayerSubsystem.cpp
@@ -2,3 +2,53 @@
 
 
 #include "LocalMultiplayerSubsystem.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "LocalMultiplayerSettings.h"
+
+void ULocalMultiplayerSubsystem::CreateAndInitPlayer(ELocalMultiplayerInputMappingType MappingType)
+{
+	const ULocalMultiplayerSettings* LocalMultiplayerSettings = GetDefault<ULocalMultiplayerSettings>();
+
+	for (int i =0; i < LocalMultiplayerSettings->GetNbKeyboardProfiles(); i++)
+	{
+		UGameplayStatics::CreatePlayer(this->GetWorld(), LastAssignedPlayerIndex, true);
+		LastAssignedPlayerIndex++;
+	}
+	LastAssignedPlayerIndex=0;
+	for (int i=0; i < LocalMultiplayerSettings->NbMaxGamepads; i++)
+	{
+		UGameplayStatics::CreatePlayer(this->GetWorld(), LastAssignedPlayerIndex, true);
+		LastAssignedPlayerIndex++;
+	}
+}
+
+int ULocalMultiplayerSubsystem::GetAssignedPlayerIndexFromKeyboardProfileIndex(int KeyboardProfileIndex)
+{
+	return PlayerIndexFromKeyboardProfileIndex[KeyboardProfileIndex];
+}
+
+int ULocalMultiplayerSubsystem::AssignNewPlayerToKeyboardProfile(int KeyboardProfileIndex)
+{
+	return -1;
+}
+
+void ULocalMultiplayerSubsystem::AssignKeyboardMapping(int PlayerIndex, int KeyboardProfileIndex,
+	ELocalMultiplayerInputMappingType MappingType) const
+{
+}
+
+int ULocalMultiplayerSubsystem::GetAssignedPlayerIndexFromGamepadDeviceID(int DeviceID)
+{
+	return -1;
+}
+
+int ULocalMultiplayerSubsystem::AssignNewPlayerToGamepadDeviceID(int DeviceID)
+{
+	return -1;
+}
+
+void ULocalMultiplayerSubsystem::AssignGamepadInputMapping(int PlayerIndex,
+	ELocalMultiplayerInputMappingType MappingType) const
+{
+}
--- SmashUE_ClearedVersion-master/Source/LocalMultiplayer/Public/LocalMultiplayerSubsystem.h
+++ SmashUE_ClearedVersion-master/Source/LocalMultiplayer/Public/LocalMultiplayerSubsystem.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "LocalMultiplayerSettings.h"
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "LocalMultiplayerSubsystem.generated.h"
 
@@ -13,4 +14,30 @@ UCLASS()
 class LOCALMULTIPLAYER_API ULocalMultiplayerSubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION()
+	void CreateAndInitPlayer(ELocalMultiplayerInputMappingType MappingType);
+
+	int GetAssignedPlayerIndexFromKeyboardProfileIndex(int KeyboardProfileIndex);
+
+	int AssignNewPlayerToKeyboardProfile(int KeyboardProfileIndex);
+
+	void AssignKeyboardMapping(int PlayerIndex, int KeyboardProfileIndex, ELocalMultiplayerInputMappingType MappingType) const;
+
+	int GetAssignedPlayerIndexFromGamepadDeviceID(int DeviceID);
+
+	int AssignNewPlayerToGamepadDeviceID(int DeviceID);
+
+	void AssignGamepadInputMapping(int PlayerIndex, ELocalMultiplayerInputMappingType MappingType) const;
+
+protected:
+	UPROPERTY()
+	uint8 LastAssignedPlayerIndex = 0;
+
+	UPROPERTY()
+	TMap<int, int> PlayerIndexFromKeyboardProfileIndex;
+
+	UPROPERTY()
+	TMap<int, int> PlayerIndexFromGamepadProfileIndex;
 };

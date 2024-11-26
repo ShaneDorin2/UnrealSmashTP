06d5a0ce5530df349449371f31cd6e1323241fa9
ShaneDorin <shane.dorin@edu.devinci.fr>
2024-11-26 10:16:21 +0100

    Local multiplayer wip

---
+++ SmashUE_ClearedVersion-master/Source/LocalMultiplayer/Private/LocalMultiplayerSettings.cpp
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LocalMultiplayerSettings.h"
+#include "EnhancedInputSubsystemInterface.h"
+#include "InputMappingContext.h"
+
+
+bool FLocalMultiplayerProfileData::ContainsKey(const FKey& Key, ELocalMultiplayerInputMappingType MappingType) const
+{
+	for (const FEnhancedActionKeyMapping& Mapping : GetIMCFromType(MappingType)->GetMappings())
+	{
+		if (Mapping.Key == Key) return true;
+	}
+	return false;
+}
+
+UInputMappingContext* FLocalMultiplayerProfileData::GetIMCFromType(ELocalMultiplayerInputMappingType MappingType) const
+{
+	switch (MappingType)
+	{
+	case ELocalMultiplayerInputMappingType::InGame:
+		return IMCInGame;
+	case ELocalMultiplayerInputMappingType::Menu:
+		return IMCMenu;
+	}
+	return nullptr;
+}
+
+int ULocalMultiplayerSettings::GetNbKeyboardProfiles() const
+{
+	return sizeof(KeyBoardProfileData);
+}
+
+int ULocalMultiplayerSettings::FindKeyboardProfileIndexFromKey(const FKey& Key,
+	ELocalMultiplayerInputMappingType MappingType) const
+{
+	for (int i = 0; i < GetNbKeyboardProfiles(); i++)
+	{
+		if (KeyBoardProfileData[i].ContainsKey(Key, MappingType))
+		{
+			return i;
+		}
+	}
+	return -1;
+}
